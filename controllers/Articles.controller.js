f4d5556af7afabc56a4ff772ee562d29ed517a4f
TypeVee <veronicaiswriting@gmail.com>
2023-10-04 14:46:28 +0100

    Fixed controller containing data manipulation

--- controllers/Articles.controller.js
+++ controllers/Articles.controller.js
@@ -4,10 +4,10 @@ exports.getArticle = ((req, res, next)=>{
     findArticle(req.params.article_id)
     .then((article)=>{
         if(article.code === '42703'){res.status(400).send()}
-        if(article.rows.length === 0){res.status(404).send()}
-        else res.status(200)
-                .send({'article': article.rows[0]}
-            )
+        if(article === "No article found"){res.status(404).send()}
+        else
+                res.status(200)
+                .send(article)
     })
     .catch((err)=>{
         next(err)
--- models/Articles.model.js
+++ models/Articles.model.js
@@ -2,6 +2,9 @@ const db = require("../db/connection")
 
 exports.findArticle = (id)=>{
     return db.query(`SELECT * FROM articles
-    WHERE article_id = ${id};`)
+    WHERE article_id = ${id};`).then((article)=>{
+        if(article.rowCount === 0){return "No article found"}
+        return {'article': article.rows[0]}
+    })
     .catch((err)=>{return err})
 }
